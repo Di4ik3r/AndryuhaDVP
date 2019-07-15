28e5257a0c35e671178cb751659d86241c1e980c
Di4ik3r <di4iker@gmail.com>
2019-07-15 23:48:42 +0300

    initial commit

---
+++ project/desktop/src/dbb/gumes/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dbb.gumes.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dbb.gumes.game.GumesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GumesGame(), config);
+	}
+}
