aced9c1e53a6637390159b2a5ffe1e4810507024
Di4ik3r <di4iker@gmail.com>
2019-07-19 12:50:39 +0300

    desktop configuration added

--- project/desktop/src/dbb/gumes/game/desktop/DesktopLauncher.java
+++ project/desktop/src/dbb/gumes/game/desktop/DesktopLauncher.java
@@ -7,6 +7,15 @@ import dbb.gumes.game.GumesGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 1280;
+		config.height = 720;
+
+		config.fullscreen = true;
+
+		config.foregroundFPS = 60;
+		config.vSyncEnabled = false;
+
 		new LwjglApplication(new GumesGame(), config);
 	}
 }
